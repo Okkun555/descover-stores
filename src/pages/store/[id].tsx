d7445f7271c81966a1fdbcb4aa30749d1e8d4912
Okkun555 <y.okuno.btm@gmail.com>
2023-03-04 10:40:12 +0900

    feat:dynamic ルーティングを検証

--- src/pages/store/[id].tsx
+++ src/pages/store/[id].tsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 
 const Store: React.FC = () => {
-  return <div>Bar Store</div>;
+  const router = useRouter();
+  const { id } = router.query;
+
+  return (
+    <>
+      <div>Bar Page {id}</div>
+      <Link href="/">Back to home</Link>
+      <Link href="/store/1111">Go to page dynamic</Link>
+    </>
+  );
 };
 
 export default Store;
